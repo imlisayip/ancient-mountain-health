2850d118d2744eafae3f287f79516bf20e05b9e6
Lisa Yip <imlisayip@gmail.com>
2023-01-18 17:18:48 -0800

    variable font sora

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts.tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["var(--font-sora)", ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
