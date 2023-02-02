a6d40fef8448d1af44e1fceadd7666893028a3f7
Lisa Yip <imlisayip@gmail.com>
2023-02-02 15:56:20 -0800

    placing comment inside braces

--- components/Acuity.tsx
+++ components/Acuity.tsx
@@ -8,7 +8,7 @@ export default function Acuity({}) {
         height="800"
         frameBorder="0"
       ></iframe>
-      // eslint-disable-next-line @next/next/no-sync-scripts
+      {// eslint-disable-next-line @next/next/no-sync-scripts}
       <script
         src="https://embed.acuityscheduling.com/js/embed.js"
         type="text/javascript"
